557af27cb8b92c2ed74dd0199f9f329dfe1a1bed
Casey Lewiston <caseylewiston@gmail.com>
2019-09-21 15:54:28 -0700

    Functions

--- api/app.js
+++ api/app.js
@@ -1,5 +1,9 @@
 var fs = require("fs");
 let header = "header";
+const util = require('util');
+var path = require('path');
+var config = require('./config');
+console.log(config.dbname);
 
 async function readHeader(readable) {
     readable.setEncoding('utf8');
@@ -10,67 +14,67 @@ async function readHeader(readable) {
     parseHeader(data);
   }
 
-  readHeader(fs.createReadStream('./img/test1.fts', {start: 0, end: 23040})).then(data => {
-    header = data; console.log("running then");
-    })
-    .catch(err => console.error(err))
-
-    console.log(header);
+ // readHeader(fs.createReadStream('./img/test1.fts', {start: 0, end: 23040})).then(data => {header = data; }).catch(err => console.error(err))
 
 function parseHeader(rawHeaderText){
-    //console.log(rawHeaderText);
     var endTag = rawHeaderText.indexOf("END") + 3;
     rawHeaderText = rawHeaderText.substr(0, endTag);
-   rawHeaderText = rawHeaderText.split("/");
-    for(var i=0; i < rawHeaderText.length; i++){
-        rawHeaderText[i] = rawHeaderText[i].replace(/\s\s+/g, ' ');
-        rawHeaderText[i] = rawHeaderText[i].split("=");
-    }
-    console.log(rawHeaderText);
-}
+    var regex1 = RegExp('.{80}','g');
+    var regex2 = RegExp('\s*=\s*');
+    var headerArr = [];
+    var array1;
+    while ((array1 = regex1.exec(rawHeaderText)) !== null) {
+       headerArr.push(array1[0]);
+     }
+    for(var i=0; i < headerArr.length; i++){
+     headerArr[i] = headerArr[i].replace(/\s\s+/g, ' ');
+     headerArr[i] = headerArr[i].replace(/\/.*/, '');
+     headerArr[i] = headerArr[i].split(regex2);
+       // rawHeaderText[i] = rawHeaderText[i].split("=");
+  }
+
+     console.log(util.inspect(headerArr, {'maxArrayLength': null}));
+
+  }
   
- 
+  function getFiles(){
+      fs.readdir('./img/', function(err, items) {
+        console.log(items);
 
-function streamTest(){
-    let stream = fs.createReadStream('./img/test1.fts', {start: 0, end: 23040});
-streamToString(stream, (data) => {
- // console.log(data);  // data is now my string variable
-});
-console.log(data);
-}
-
-
-function getHeader(filename){
-    var stream;
-    stream = fs.createReadStream(filename, { start: 0, end: 23040});
-    stream.on("data", function(data) {
-        var chunk = data.toString();
-        var endTag = chunk.indexOf("END") + 3;
-        chunk = chunk.substr(0, endTag);
-        var chunk = chunk.split(" /");
-        for(var i=0; i < chunk.length; i++){
-            chunk[i] = chunk[i].replace(/\s\s+/g, ' ');
-            string.push(chunk[i]);
-            chunk[i] = chunk[i].split("=");
+        for (var i=0; i<items.length; i++) {
+            console.log(items[i]);
         }
-        
-    });
-console.log(typeof(string));
-}
-
-function streamToString(stream, cb) {
-    const chunks = [];
-    stream.on('data', (chunk) => {
-      chunks.push(chunk.toString());
-    });
-    stream.on('end', () => {
-      cb(chunks.join(''));
     });
   }
 
+  var getFiles = function(dir, done) {
+    var results = [];
+    fs.readdir(dir, function(err, list) {
+      if (err) return done(err);
+      var pending = list.length; if(!pending)done(null, results); 
+      if (!pending) return done(null, results);
+      list.forEach(function(file) {
+        file = path.resolve(dir, file);
+        fs.stat(file, function(err, stat) {
+          if (stat && stat.isDirectory()) {
+            getFiles(file, function(err, res) {
+              results = results.concat(res);
+              if (!--pending) done(null, results);
+            });
+          } else {
+            results.push(file);
+            if (!--pending) done(null, results);
+          }
+        });
+      });
+    });
+  };
 
-
-//streamTest();
-
-
+  getFiles('./img/', function(err, results) {
+    if (err) throw err;
+    for(var i=0; i < results.length; i++){
+      results[i] = results[i].replace(/^(.*?)\img/, '');      
+    }
+    console.log(results);
+  });
 
