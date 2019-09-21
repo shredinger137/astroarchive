31c3bb1cc5346a4accff41829ac88b3d2bced37b
Shredinger137 <caseylewiston@gmail.com>
2019-09-21 13:41:03 -0700

    Stuff n Thangs

--- api/app.js
+++ api/app.js
@@ -1,11 +1,76 @@
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'gtn',
-  password: '',
-  database: 'gortarchive'
+var fs = require("fs");
+let header = "header";
+
+async function readHeader(readable) {
+    readable.setEncoding('utf8');
+    let data = '';
+    for await (const chunk of readable) {
+      data += chunk;
+    }
+    parseHeader(data);
+  }
+
+  readHeader(fs.createReadStream('./img/test1.fts', {start: 0, end: 23040})).then(data => {
+    header = data; console.log("running then");
+    })
+    .catch(err => console.error(err))
+
+    console.log(header);
+
+function parseHeader(rawHeaderText){
+    //console.log(rawHeaderText);
+    var endTag = rawHeaderText.indexOf("END") + 3;
+    rawHeaderText = rawHeaderText.substr(0, endTag);
+   rawHeaderText = rawHeaderText.split("/");
+    for(var i=0; i < rawHeaderText.length; i++){
+        rawHeaderText[i] = rawHeaderText[i].replace(/\s\s+/g, ' ');
+        rawHeaderText[i] = rawHeaderText[i].split("=");
+    }
+    console.log(rawHeaderText);
+}
+  
+ 
+
+function streamTest(){
+    let stream = fs.createReadStream('./img/test1.fts', {start: 0, end: 23040});
+streamToString(stream, (data) => {
+ // console.log(data);  // data is now my string variable
 });
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Connected!');
-});
+console.log(data);
+}
+
+
+function getHeader(filename){
+    var stream;
+    stream = fs.createReadStream(filename, { start: 0, end: 23040});
+    stream.on("data", function(data) {
+        var chunk = data.toString();
+        var endTag = chunk.indexOf("END") + 3;
+        chunk = chunk.substr(0, endTag);
+        var chunk = chunk.split(" /");
+        for(var i=0; i < chunk.length; i++){
+            chunk[i] = chunk[i].replace(/\s\s+/g, ' ');
+            string.push(chunk[i]);
+            chunk[i] = chunk[i].split("=");
+        }
+        
+    });
+console.log(typeof(string));
+}
+
+function streamToString(stream, cb) {
+    const chunks = [];
+    stream.on('data', (chunk) => {
+      chunks.push(chunk.toString());
+    });
+    stream.on('end', () => {
+      cb(chunks.join(''));
+    });
+  }
+
+
+
+//streamTest();
+
+
+
--- phptest.php
+++ phptest.php
@@ -1,12 +1,12 @@
 <?php
-    function getMetaData(filename) {
+    function getMetaData($filename) {
     $head = file_get_contents($filename, FALSE, NULL, 0, 23040);
     $endpost = strpost($testdata, 'END') + 3;
     $head_arr = explode(" /", $testdata);
     return $head_arr;
     }
 
-$testdata_arr = getMetaData('./test.fts')
+$testdata_arr = getMetaData('./test.fts');
 foreach ($testdata_arr as $line) {
 echo "<br>";
 echo $line;
