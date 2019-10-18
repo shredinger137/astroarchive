3d44beadbf339e9d5578857c68fb470699d394e2
Shredinger137 <caseylewiston@gmail.com>
2019-10-18 13:46:43 -0700

    Adding .zip download API

--- api/app.js
+++ api/app.js
@@ -17,13 +17,11 @@ var cron = require('node-cron');
 var page = 0;
 var count = 50;
 var query = {};
+const AdmZip = require('adm-zip');
 
 cron.schedule('* 5 * * *', () => {
  getEntries();
 });
-//AAAAAH
-//TODO: Turn filters into an encoded object. Include pages. Pass it in URI from frontend.
-//GENERALIZE AND SHIT.
 
 app.get('/', function(req, res){
   var data = [];
@@ -99,6 +97,59 @@ app.get('/stats', function(req, res){
     });
   });
 });
+
+
+app.get('/downloadAll', function(req, res){
+  var data = [];
+  res.statusCode = 200;
+  var query = {};
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  if(req.query.dateFrom && req.query.dateTo){
+    query['DATEOBS'] = {$gte: +req.query.dateFrom, $lte: +req.query.dateTo};
+  } else {
+    if(req.query.dateFrom){
+      query['DATEOBS'] = {$gte: +req.query.dateFrom};
+    }
+    if(req.query.dateTo){
+      query['DATEOBS'] = {$lte: +req.query.dateTo};
+    }
+  }
+
+  if(req.query.object){
+    query['OBJECT'] = req.query.object;
+  }
+
+  mongo.connect(mongourl, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("gortarchive");
+    dbo.collection("gortarchive").find(query,{ projection: {
+      _id: 0, filename: 1}}).sort( {DATEOBS: -1} ).toArray(function(err, result) {
+      if (err) {
+        throw err
+      }
+
+      if (result) {
+        const zip = new AdmZip();
+        console.log(result);
+        for(var i=0; i<result.length; i++){
+          console.log(result[i]['filename']);
+          zip.addLocalFile(result[i]['filename']);
+        }
+        const downloadName = `${Date.now()}.zip`;
+        const data = zip.toBuffer();
+        res.set('Content-Type','application/octet-stream');
+        res.set('Content-Disposition',`attachment; filename=${downloadName}`);
+        res.set('Content-Length',data.length);
+        res.send(data);
+
+        //res.send({result})
+        
+        ;};
+    });
+  });
+});
+
 
 
 
