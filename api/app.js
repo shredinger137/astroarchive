6cc2447df596c417587fb1749bc0d1244ce97984
Casey Lewiston <caseylewiston@gmail.com>
2019-11-01 19:02:57 -0700

    Cleanup

--- api/app.js
+++ api/app.js
@@ -1,22 +1,11 @@
 var fs = require("fs");
-let header = [];
-const util = require('util');
 var path = require('path');
-var config = require('./config');
-var mysql = require("mysql");
 var mongo = require('mongodb');
 var mongourl = 'mongodb://localhost/gortarchive';
 var headerobj = {};
-let headerArr;
-let entries = [];
-var http = require('http');
-var cors = require('cors');
 var express=require('express');
 var app = express();
 var cron = require('node-cron');
-var page = 0;
-var count = 50;
-var query = {};
 const AdmZip = require('adm-zip');
 
 cron.schedule('* 5 * * *', () => {
@@ -100,63 +89,6 @@ app.get('/stats', function(req, res){
     });
   });
 });
-
-//This has become a blocking function.
-//TODO: Async this or move to a separate instance.
-//use this same app for easy testing and demo.
-
-app.get('/downloadAll', function(req, res){
-  var data = [];
-  res.statusCode = 200;
-  var query = {};
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  if(req.query.dateFrom && req.query.dateTo){
-    query['DATEOBS'] = {$gte: +req.query.dateFrom, $lte: +req.query.dateTo};
-  } else {
-    if(req.query.dateFrom){
-      query['DATEOBS'] = {$gte: +req.query.dateFrom};
-    }
-    if(req.query.dateTo){
-      query['DATEOBS'] = {$lte: +req.query.dateTo};
-    }
-  }
-
-  if(req.query.object){
-    query['OBJECT'] = req.query.object;
-  }
-
-  mongo.connect(mongourl, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("gortarchive");
-    dbo.collection("gortarchive").find(query,{ projection: {
-      _id: 0, filename: 1}}).sort( {DATEOBS: -1} ).toArray(function(err, result) {
-      if (err) {
-        throw err
-      }
-
-      if (result) {
-        const zip = new AdmZip();
-        console.log(result);
-        for(var i=0; i<result.length; i++){
-          console.log(result[i]['filename']);
-          zip.addLocalFile(result[i]['filename']);
-        }
-        const downloadName = `${Date.now()}.zip`;
-        const data = zip.toBuffer();
-        res.set('Content-Type','application/octet-stream');
-        res.set('Content-Disposition',`attachment; filename=${downloadName}`);
-        res.set('Content-Length',data.length);
-        res.send(data);
-
-        //res.send({result})
-        
-        ;};
-    });
-  });
-});
-
-
 
 
 function addFile(filename, properties){
